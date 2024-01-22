4fd72f83aa37ad5d590eb98ace598d8644b81660
Swaroopa302 <swaroopathanda302@gmail.com>
2024-01-22 23:41:28 +0530

    TestExp1ClassA
    Is it working

---
+++ MyFirstGitProj/src/com/exp1/ClassA.java
@@ -0,0 +1,11 @@
+package com.exp1;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Jesus");
+System.out.println("Lord");
+	}
+
+}
